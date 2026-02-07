d6d7a84d17df45a4b0589a368b6118e26662008f
Hanh Le Doan <doanxhanh05@gmail.com>
2026-02-08 00:00:23 +0700

    30

--- src/server.js
+++ src/server.js
@@ -3,7 +3,8 @@ const express = require('express')
 const path = require('path')
 const configViewEngine = require('./config/viewEngine')
 const webRoutes = require('./routes/web')
-
+const mysql = require('mysql2')
+const { log } = require('console')
 const app = express()
 const port = process.env.PORT || 8888
 const hostname = process.env.HOST_NAME
@@ -13,13 +14,27 @@ configViewEngine(app)
 
 
 //khai báo route
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.use('/', webRoutes)
 
-app.get('/abc', (req, res) => {
-    res.render('sample.ejs')
-})
+//test  connection
+//create the connection to database 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3307,
+    user: 'root', //defaul: empty
+    password: '123456',
+    database: 'hoidanit'
+});
+
+//simple query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log(">>>results=  ", results);
+        console.log(">>>fields= ", fields);
+
+    }
+)
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
