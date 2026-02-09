7987a77e3ab8f1dcfa44d09a81aca69f6ead3e2e
Hanh Le Doan <doanxhanh05@gmail.com>
2026-02-09 13:15:30 +0700

    32

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,17 @@
+const connection = require('../config/database')
+
 const getHomepage = (req, res) => {
-    res.send('hello hanh dep trai nhat the gioi')
+    let users = [];
+    connection.query(
+        'select * from Users u',
+        function (err, results, fields) {
+            users = results;
+            console.log(">>>results home page=  ", results);
+
+            // console.log(">>>check users: ", users);
+            res.send(JSON.stringify(users))
+        })
+
 }
 const getABC = (req, res) => {
     res.send("this is ABC page")
@@ -7,4 +19,5 @@ const getABC = (req, res) => {
 const getHanhdeptrai = (req, res) => {
     res.render('sample.ejs')
 }
+
 module.exports = { getHomepage, getABC, getHanhdeptrai }
