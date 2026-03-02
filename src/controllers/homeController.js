f82d87278d154f456a86c36a05d3432e3df474bf
Hanh Le Doan <doanxhanh05@gmail.com>
2026-03-02 23:17:45 +0700

    39

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,23 +9,35 @@ const getABC = (req, res) => {
 const getHanhdeptrai = (req, res) => {
     res.render('sample.ejs')
 }
-const postCreateUser = (req, res) => {
-    console.log(">>> req.body:", req.body);
-    // let email = req.body.email;
-    // let name = req.body.myname;
-    // let city = req.body.city;
+const postCreateUser = async (req, res) => {
+    // console.log(">>> req.body:", req.body);
+    let email = req.body.email;
+    let name = req.body.myname;
+    let city = req.body.city;
 
-    let { email, name, city } = req.body;
+    // let { email, name, city } = req.body;
 
-    connection.query(
+
+    let [results, fields] = await connection.query(
         `INSERT INTO Users (email, name, city) 
         VALUES (?, ?, ?)`,
-        [email, name, city],
-        function (err, results) {
-            console.log(results);
-            res.send('create user succeed !')
-        }
+        [email, name, city]
     );
+    res.send("create success")
+    // connection.query(
+    //     'select * from Users u',
+    //     function (err, results, fields) {
+    //         console.log(">>result = ", results);
+    //     }
+
+    // )
+    // const [results, fields] = await connection.query('select * from Users u');
+    // console.log(">>> check results: ", results);
+
 }
 
-module.exports = { getHomepage, getABC, getHanhdeptrai, postCreateUser }
+const getCreatePage = (req, res) => {
+    res.render('create.ejs')
+}
+
+module.exports = { getHomepage, getABC, getHanhdeptrai, postCreateUser, getCreatePage }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { getHomepage, getABC, postCreateUser, getHanhdeptrai } = require('../controllers/homeController')
+const { getHomepage, getABC, postCreateUser, getHanhdeptrai, getCreatePage } = require('../controllers/homeController')
 const router = express.Router()
 //khai báo route
 router.get('/', getHomepage)
 router.get('/abc', getABC)
+
+router.get('/create', getCreatePage)
 router.post('/create-user', postCreateUser);
 
 module.exports = router;
